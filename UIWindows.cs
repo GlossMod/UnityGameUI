3a62dd98a5fdcf9ab6f8c6a2e096e14da0da9634
小莫 <xiaomo1433675239@outlook.com>
2022-09-28 10:58:29 +0800

    1.添加弹出下拉框 2.修复有时弹窗会被其他UI覆盖的问题

--- UIWindows.cs
+++ UIWindows.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
@@ -19,6 +20,9 @@ namespace UnityGameUI
         {
             GameObject canvas = UIControls.createUICanvas();    // 创建画布
             Object.DontDestroyOnLoad(canvas);
+            // 设置置顶显示
+            canvas.GetComponent<Canvas>().overrideSorting = true;
+            canvas.GetComponent<Canvas>().sortingOrder = 100;
 
             GameObject uiPanel = UIControls.createUIPanel(canvas, "70", "300", null);  // 创建面板
             uiPanel.GetComponent<Image>().color = UIControls.HTMLString2Color("#37474FFF"); // 设置背景颜色
@@ -51,6 +55,64 @@ namespace UnityGameUI
             closeButton.GetComponent<RectTransform>().sizeDelta = new Vector2(20, 20);
             // 字体颜色为白色
             closeButton.GetComponentInChildren<Text>().color = UIControls.HTMLString2Color("#FFFFFFFF");
+        }
+
+        // 带下拉框的对话框
+        public static void SpawnDropdownDialog(string prompt, string title, List<string> options,  Action<int> onFinish)
+        {
+            GameObject canvas = UIControls.createUICanvas();    // 创建画布
+            Object.DontDestroyOnLoad(canvas);
+            // 设置置顶显示
+            canvas.GetComponent<Canvas>().overrideSorting = true;
+            canvas.GetComponent<Canvas>().sortingOrder = 100;
+            
+
+            GameObject uiPanel = UIControls.createUIPanel(canvas, "70", "300", null);  // 创建面板
+            uiPanel.GetComponent<Image>().color = UIControls.HTMLString2Color("#37474FFF"); // 设置背景颜色
+            
+
+            // 创建标题
+            Sprite txtBgSprite = UIControls.createSpriteFrmTexture(UIControls.createDefaultTexture("#7AB900FF"));
+            GameObject uiText = UIControls.createUIText(uiPanel, txtBgSprite, "#FFFFFFFF");
+            uiText.GetComponent<Text>().text = prompt;
+            uiText.GetComponent<RectTransform>().localPosition = new Vector3(0, 10, 0);
+
+            // 创建下拉框
+            Sprite dropdownBgSprite = UIControls.createSpriteFrmTexture(UIControls.createDefaultTexture("#212121FF"));      // 背景颜色
+            Sprite dropdownScrollbarSprite = UIControls.createSpriteFrmTexture(UIControls.createDefaultTexture("#8C9EFFFF"));   // 滚动条颜色 (如果有的话
+            Sprite dropdownDropDownSprite = UIControls.createSpriteFrmTexture(UIControls.createDefaultTexture("#212121FF"));    // 框右侧小点的颜色
+            Sprite dropdownCheckmarkSprite = UIControls.createSpriteFrmTexture(UIControls.createDefaultTexture("#8C9EFFFF"));   // 选中时的颜色
+            Sprite dropdownMaskSprite = UIControls.createSpriteFrmTexture(UIControls.createDefaultTexture("#E65100FF"));        // 不知道是哪的颜色
+            Color LabelColor = UIControls.HTMLString2Color("#EFEBE9FF");
+            GameObject uiDropDown = UIControls.createUIDropDown(uiPanel, dropdownBgSprite, dropdownScrollbarSprite, dropdownDropDownSprite, dropdownCheckmarkSprite, dropdownMaskSprite, options, LabelColor);
+            Object.DontDestroyOnLoad(uiDropDown);
+            uiDropDown.GetComponent<RectTransform>().localPosition = new Vector3(-50, -10, 0);
+
+            int m_call = 0;
+
+            // 下拉框选中时触发方法
+            uiDropDown.GetComponent<Dropdown>().onValueChanged.AddListener((int call) =>
+            {
+                m_call = call;
+            });
+
+            // 创建确定按钮
+            GameObject uiButton = UIControls.createUIButton(uiPanel, "#8C9EFFFF", title, () =>
+            {
+                onFinish(m_call);
+                Object.Destroy(canvas);
+            }, new Vector3(100, -10, 0));
+
+            // 创建关闭按钮
+            GameObject closeButton = UIControls.createUIButton(uiPanel, "#B71C1CFF", "X", () =>
+            {
+                Object.Destroy(canvas);
+            }, new Vector3(350 / 2 - 10, 70 / 2 - 10, 0));
+            // 设置closeButton宽高
+            closeButton.GetComponent<RectTransform>().sizeDelta = new Vector2(20, 20);
+            // 字体颜色为白色
+            closeButton.GetComponentInChildren<Text>().color = Color.white;
+
         }
 
     }
